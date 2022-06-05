d65de1908f64e65fea259eba206f14828e57095a
Tobias Mahnert <tobias.mahnert@nano.com.au>
2022-06-05 20:48:09 +1000

    Arrays and got

---
+++ arrays/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+const (
+	winter = 1
+	summer = 3
+	yearly = winter + summer
+)
+
+func main() {
+	var books [yearly]string
+	books[0] = "Kafkas revenge"
+	books[1] = "mehg"
+	books[2] = "Everythingship"
+	books[3] += books[0] + " 2nd Edition"
+	fmt.Printf("books : %#v\n", books)
+
+	var (
+		wBooks [winter]string
+		sBooks [summer]string
+	)
+
+	wBooks[0] = books[0]
+	// sBooks[0] = books[1]
+	// sBooks[1] = books[2]
+	// sBooks[2] = books[3]
+
+	for i := range sBooks {
+		sBooks[i] = books[i+1]
+	}
+
+	// for _, v := range sBooks {
+	// 	v += "won't effect"
+	// }
+
+	fmt.Printf("\nwinter : %#v\n", wBooks)
+	fmt.Printf("\nsummer : %#v\n", sBooks)
+
+	var published [len(books)]bool
+
+	published[0] = true
+	published[len(books)-1] = true
+
+	fmt.Println("\nPublished Books:")
+	for i, ok := range published {
+		if ok {
+			fmt.Printf("+ %s\n", books[i])
+		}
+	}
+
+}
---
+++ goto/goto.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+
+loop:
+	if i < 3 {
+		fmt.Println("looping")
+		i++
+		goto loop
+	}
+	fmt.Println("done")
+}
