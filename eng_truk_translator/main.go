f885e634e0832c433273cffbd3447e5b3232f1fb
Tobias Mahnert <tobias.mahnert@nano.com.au>
2022-06-19 18:56:57 +1000

    Using maps in go

---
+++ eng_truk_translator/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("[english word] -> [turkish word]")
+		return
+	}
+	query := args[0]
+	dict := map[string]string{
+		"up":       "yukarı",
+		"down":     "aşağı",
+		"left":     "sol",
+		"right":    "sağ",
+		"forward":  "ileri",
+		"backward": "geri",
+		"stop":     "dur",
+		"start":    "başlat",
+		"yes":      "evet",
+		"no":       "hayır",
+		"on":       "açık",
+		"off":      "kapalı",
+	}
+
+	if value, ok := dict[query]; ok {
+		fmt.Printf("%s -> %s\n", query, value)
+		fmt.Printf("# of keys %d\n", len(dict))
+		return
+	}
+
+	fmt.Println("Not found")
+}
