18c4ab61531fea06660654c9f451f8a73b5cca71
Tobias Mahnert <tobias.mahnert@nano.com.au>
2022-06-25 17:36:55 +1000

    Added log parser struct

--- log_parser/main.go
+++ log_parser/main.go
@@ -9,51 +9,61 @@ import (
 	"strings"
 )
 
-func main() {
-	var (
-		sum     map[string]int
-		domains []string
-		total   int
-		lines   int
-	)
+type result struct {
+	domain string
+	visits int
+}
 
-	sum = make(map[string]int)
+type parser struct {
+	sum     map[string]result
+	domains []string
+	total   int
+	lines   int
+}
+
+func main() {
+
+	p := parser{sum: make(map[string]result)}
 
 	in := bufio.NewScanner(os.Stdin)
 
 	for in.Scan() {
-		lines++
+		p.lines++
 		fields := strings.Fields(in.Text())
 
 		if len(fields) != 2 {
-			fmt.Printf("wrong input: %v (line #%d\n", fields, lines)
+			fmt.Printf("wrong input: %v (line #%d\n", fields, p.lines)
 			return
 		}
 
 		domain := fields[0]
 		visits, err := strconv.Atoi(fields[1])
 		if visits < 0 || err != nil {
-			fmt.Printf("wrong input: %q (line #%d)\n", fields[1], lines)
+			fmt.Printf("wrong input: %q (line #%d)\n", fields[1], p.lines)
 			return
 		}
 
-		if _, ok := sum[domain]; !ok {
-			domains = append(domains, domain)
+		if _, ok := p.sum[domain]; !ok {
+			p.domains = append(p.domains, domain)
 		}
 
-		total += visits
-		sum[domain] += visits
+		p.total += visits
+		p.sum[domain] = result{
+			domain: domain,
+			visits: visits + p.sum[domain].visits,
+		}
+
 	}
 
 	fmt.Printf("%-30s %10s\n", "Domain", "Visits")
 	fmt.Println(strings.Repeat("-", 42))
 
-	sort.Strings(domains)
-	for _, domain := range domains {
-		visits := sum[domain]
-		fmt.Printf("%-30s %10d\n", domain, visits)
+	sort.Strings(p.domains)
+	for _, domain := range p.domains {
+		parsed := p.sum[domain]
+		fmt.Printf("%-30s %10d\n", domain, parsed.visits)
 	}
-	fmt.Printf("\n%-30s %10d\n", "Total", total)
+	fmt.Printf("\n%-30s %10d\n", "Total", p.total)
 
 	if err := in.Err(); err != nil {
 		fmt.Println("> Err:", err)
